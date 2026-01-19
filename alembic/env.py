a42f7d6aeb467af8d214a03a4438b5ccd1b22bcb
Aidan <aidanhuang10@gmail.com>
2026-01-19 12:18:22 -0700

    Changes: - Moved all data that need to be protected into our .env file and updated the .env.example file to include steps and changes for the required data

--- alembic/env.py
+++ alembic/env.py
@@ -9,6 +9,9 @@ from app.db.base import Base
 from app.models.user import User
 from app.models.refresh_token import RefreshToken
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
--- app/core/config.py
+++ app/core/config.py
@@ -4,17 +4,16 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     PROJECT_NAME: str = "PSCC Analytics Backend"
     API_V1_PREFIX: str = "/api/v1"
-    
+
     DATABASE_URL: str
 
     # 🔐 JWT-related settings
-    JWT_SECRET: str = "change-this-to-a-long-random-string"
-    JWT_ALG: str = "HS256"
-    ACCESS_TOKEN_MINUTES: int = 15  # 15 minutes for now
-    REFRESH_TOKEN_DAYS: int = 30  # 30 days for now
+    JWT_SECRET: str
+    JWT_ALG: str
+    ACCESS_TOKEN_MINUTES: int
+    REFRESH_TOKEN_DAYS: int
 
     class Config:
         env_file = ".env"
-
 
 settings = Settings()

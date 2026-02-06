c4c430f0cf7f42166fc4ed11b0683dbffde6c492
Aidan <aidanhuang10@gmail.com>
2026-02-06 13:42:53 -0700

    Refactor: - re-organized the Settings class and it not longer takes hardcoded input for JWT_ALG and tokens
    Changes:
    - Uses psycopg instead of psycopg2

--- app/core/config.py
+++ app/core/config.py
@@ -7,22 +7,23 @@ class Settings(BaseSettings):
     DB_NAME: str
     DB_USER: str
     DB_PASSWORD: str
+    DB_SSL: str = "require"
 
     JWT_SECRET: str
-    JWT_ALG: str = "HS256"
-    ACCESS_TOKEN_MINUTES: int = 15
-    REFRESH_TOKEN_DAYS: int = 30
+    JWT_ALG: str 
+    ACCESS_TOKEN_MINUTES: int
+    REFRESH_TOKEN_DAYS: int
 
     @computed_field
     @property
     def database_url(self) -> str:
         return (
-            f"postgresql+psycopg2://{self.DB_USER}:"
+            f"postgresql+psycopg://{self.DB_USER}:"
             f"{self.DB_PASSWORD}@{self.DB_HOST}:"
             f"{self.DB_PORT}/{self.DB_NAME}"
         )
 
     class Config:
-        env_file = ".env"
+        env_file = ".env",
 
 settings = Settings()
